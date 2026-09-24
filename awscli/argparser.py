4013fedeb90dde6a8c6d67b972b243153b4549ca
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-24 18:34:28 -0400

    Muna4029__diff__id_143

--- awscli/argparser.py
+++ awscli/argparser.py
@@ -42,7 +42,7 @@ class CommandAction(argparse.Action):
 
     def __init__(self, option_strings, dest, command_table, **kwargs):
         self.command_table = command_table
-        super(CommandAction, self).__init__(
+        super().__init__(
             option_strings, dest, choices=self.choices, **kwargs
         )
 
@@ -91,8 +91,18 @@ class CLIArgParser(argparse.ArgumentParser):
                 msg.extend(extra)
             raise argparse.ArgumentError(action, '\n'.join(msg))
 
+    def _check_help(self, action):
+        """Override to disable help string validation.
+
+        Python 3.14 added _check_help which validates help strings with
+        %-style expansion, failing on literal "%" characters in help text.
+        AWS CLI help text can contain literal "%" characters, so we disable
+        this validation by making this method a no-op.
+        """
+        pass
+
     def parse_known_args(self, args, namespace=None):
-        parsed, remaining = super(CLIArgParser, self).parse_known_args(
+        parsed, remaining = super().parse_known_args(
             args, namespace
         )
         terminal_encoding = getattr(sys.stdin, 'encoding', 'utf-8')
@@ -141,7 +151,7 @@ class MainArgParser(CLIArgParser):
         argument_table,
         prog=None,
     ):
-        super(MainArgParser, self).__init__(
+        super().__init__(
             formatter_class=self.Formatter,
             add_help=False,
             conflict_handler='resolve',
@@ -174,7 +184,7 @@ class MainArgParser(CLIArgParser):
 
 class ServiceArgParser(CLIArgParser):
     def __init__(self, operations_table, service_name):
-        super(ServiceArgParser, self).__init__(
+        super().__init__(
             formatter_class=argparse.RawTextHelpFormatter,
             add_help=False,
             conflict_handler='resolve',
@@ -196,7 +206,7 @@ class ArgTableArgParser(CLIArgParser):
         # command_table is an optional subcommand_table.  If it's passed
         # in, then we'll update the argparse to parse a 'subcommand' argument
         # and populate the choices field with the command table keys.
-        super(ArgTableArgParser, self).__init__(
+        super().__init__(
             formatter_class=self.Formatter,
             add_help=False,
             usage=USAGE,
@@ -224,7 +234,7 @@ class ArgTableArgParser(CLIArgParser):
             namespace.help = 'help'
             return namespace, []
         else:
-            return super(ArgTableArgParser, self).parse_known_args(
+            return super().parse_known_args(
                 args, namespace
             )
 
@@ -239,9 +249,7 @@ class SubCommandArgParser(ArgTableArgParser):
     """
 
     def parse_known_args(self, args, namespace=None):
-        parsed_args, remaining = super(
-            SubCommandArgParser, self
-        ).parse_known_args(args, namespace)
+        parsed_args, remaining = super().parse_known_args(args, namespace)
         if getattr(parsed_args, 'subcommand', None) is not None:
             new_args = self._remove_subcommand(args, parsed_args)
             return new_args, parsed_args.subcommand
